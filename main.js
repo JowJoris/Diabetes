17c0daed262eb181083818dc48aeeaa82c198fd8
Joris Huinink <joris.huinink@gmail.com>
2020-01-29 16:20:50 +0100

    Starting with headers

---
+++ main.js
@@ -0,0 +1,10 @@
+let app = new Vue({
+      el: '#app',
+      data: {
+        subjects: {
+          {},
+          {}
+        }
+        headerNL = "Welkom bij deze uitleg over diabetes"
+        headerTK = "Diyabet hakkındaki bu açıklamaya hoş geldiniz"
+      };
