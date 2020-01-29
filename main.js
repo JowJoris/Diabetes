d98e2b98245bf5cea63efc37c9fda198e5d8fc7c
Joris Huinink <joris.huinink@gmail.com>
2020-01-29 19:01:44 +0100

    Added this.

--- main.js
+++ main.js
@@ -12,15 +12,16 @@ var pictures = new Vue({
   el: '#pictures',
   data: {
     pictures: [{
-      infoNL: '1',
+      infoNL: 'Welkom bij deze interactieve website met informatie over diabetes. \n' +
+      'Ik Ceres Jansen en ? van de opleiding HBO-V willen met behulp van deze meer informatie verlenen over diabetes',
       infoTK: '1',
-      headerNL: '1',
-      headerTK: '1'
+      headerNL: 'Klik op de insulinepen',
+      headerTK: 'İnsülin kalemine tıklayın'
     }, {
-      infoNL: '2',
-      infoTK: '2',
-      headerNL: '2',
-      headerTK: '2'
+      infoNL: "",
+      infoTK: "",
+      headerNL: "Klik op het gezonde eten",
+      headerTK: "Sağlıklı yiyeceklere tıklayın"
     }]
   },
   methods: {
@@ -32,10 +33,10 @@ var pictures = new Vue({
     },
     pictureClicked: function(i) {
       if (this.checkCorrectPicture(i)) {
-        text.textNL += pictures[i].infoNL;
-        text.textTK += pictures[i].infoTK;
-        header.nl = pictures.pictures[i].headerNL;
-        header.tk = pictures[i].headerTK;
+        text.textNL += this.pictures[i].infoNL;
+        text.textTK += this.pictures[i].infoTK;
+        header.nl = this.pictures[i].headerNL;
+        header.tk = this.pictures[i].headerTK;
         nr = nr+1;
       }
     }
